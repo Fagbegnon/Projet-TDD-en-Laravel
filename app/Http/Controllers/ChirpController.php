8da366ed6aaaff18eff449c54476059e71f1925d
Fagbegnon <brufagbegnon@gmail.com>
2024-12-08 19:20:05 +0100

    Affichage des Chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -18,7 +18,9 @@ class ChirpController extends Controller
      */
     public function index(): View
     {
-        return view('chirps.index');
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get(),
+        ]);
     }
 
     /**
